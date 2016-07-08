550852dadd02c27bffbce2d1b57ee8f9af082160
Surbma <peter@surbma.hu>
2016-07-08 23:29:37 +0200

    2.5.0
    - Tested up to WordPress 4.5 version.
    - Updated to Font Awesome 4.6.3 version.

--- readme.txt
+++ readme.txt
@@ -1,14 +1,14 @@
 === Surbma - Font Awesome ===
 Contributors: Surbma
 Donate link: http://surbma.com/
-Tags: font awesome, icon font, icons
+Tags: font awesome, fontawesome, icon font, icons
 Requires at least: 3.0
-Tested up to: 4.3
-Stable tag: 2.4.0
+Tested up to: 4.5
+Stable tag: 2.5.0
 License: GPLv2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Font Awesome 4.3 - The iconic font and CSS toolkit
+Font Awesome - The iconic font and CSS toolkit
 
 == Description ==
 
@@ -38,6 +38,11 @@ Here you can see them: <a href="http://fortawesome.github.io/Font-Awesome/icons/
 It is the reverse version of my last name. ;)
 
 == Changelog ==
+
+= 2.5.0 =
+
+- Tested up to WordPress 4.5 version.
+- Updated to Font Awesome 4.6.3 version.
 
 = 2.4.0 =
 
--- surbma-font-awesome.php
+++ surbma-font-awesome.php
@@ -5,7 +5,7 @@ Plugin Name: Surbma - Font Awesome
 Plugin URI: http://surbma.com/wordpress-plugins/
 Description: Font Awesome - The iconic font and CSS toolkit
 
-Version: 2.4.0
+Version: 2.5.0
 
 Author: Surbma
 Author URI: http://surbma.com/
@@ -17,13 +17,11 @@ Domain Path: /languages/
 */
 
 // Prevent direct access to the plugin
-if ( !defined( 'ABSPATH' ) ) {
-	die( 'Good try! :)' );
-}
+if ( !defined( 'ABSPATH' ) ) exit( 'Good try! :)' );
 
 // Localization
 function surbma_font_awesome_init() {
-	load_plugin_textdomain( 'surbma-font-awesome', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	load_plugin_textdomain( 'surbma-font-awesome', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
 }
 add_action( 'plugins_loaded', 'surbma_font_awesome_init' );
 
@@ -33,7 +31,7 @@ function surbma_font_awesome_enqueue_scripts() {
 	if ( wp_style_is( $handle, $list ) ) {
 		wp_dequeue_style( $handle );
 	}
-	wp_enqueue_style( 'surbma-font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css', false, '4.4.0' );
+	wp_enqueue_style( 'surbma-font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css', false, '4.6.3' );
 }
 add_action( 'wp_enqueue_scripts', 'surbma_font_awesome_enqueue_scripts', 25 );
 
